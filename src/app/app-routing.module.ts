274f3faba28d8f0cd54a0662db2dfeb0d82279c7
Jan Pereira Zambrano <jnpb@gft.com>
2021-05-19 12:07:33 +0200

    Nuevos Componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BodyComponent } from './shared/body/body.component';
+import { TableComponent } from './shared/table/table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BodyComponent },
+  { path: 'table', component: TableComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './shared/body/body.component';
+import { TableComponent } from './shared/table/table.component';
+import { HomeComponent } from './shared/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    TableComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
